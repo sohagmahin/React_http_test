b519e887244c7bfa9ea8e3fe4066368fa0132d9f
sohagmahin <sohagmahin@gmail.com>
2021-01-11 01:19:37 +0600

    initial commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+import Axios from 'axios';
+
+class Blog extends Component {
+
+    state = {
+        posts: [],
+    }
+    componentDidMount(){
+       Axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            this.setState({posts: response.data})
+            console.log(response);
+        })
+    }
+    render () {
+
+        const posts = this.state.posts.map(post=>{
+            return   <Post key = {post.id} title = {post.title}/>;
+        })
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
