2e869b52dea106a6fa83413bb394e4e3aa8e10b6
sohagmahin <sohagmahin@gmail.com>
2021-01-18 00:21:37 +0600

    L9 v9 Posting data to the server

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './NewPost.css';
+import Axios from 'axios';
 
 class NewPost extends Component {
     state = {
@@ -9,20 +10,30 @@ class NewPost extends Component {
         author: 'Max'
     }
 
-    render () {
+    sentPost = () => {
+        let data = {
+            'title': this.state.title,
+            'content': this.state.content,
+            'author': 'SOHAG',
+        }
+        Axios.post("https://jsonplaceholder.typicode.com/posts/", data)
+            .then(response => console.log(response.data));
+    }
+
+    render() {
         return (
             <div className="NewPost">
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
                 <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({ content: event.target.value })} />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                <select value={this.state.author} onChange={(event) => this.setState({ author: event.target.value })}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.sentPost}>Add Post</button>
             </div>
         );
     }
