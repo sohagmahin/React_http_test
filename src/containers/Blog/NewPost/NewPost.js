6cc33b08ee52c741df9f49b5a349e76bd8982b52
sohagmahin <sohagmahin@gmail.com>
2021-02-12 20:24:15 +0600

    L11 v11 using routing-related props

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -20,6 +20,10 @@ class NewPost extends Component {
             .then(response => console.log(response.data));
     }
 
+    componentDidMount() {
+        console.log(this.props);
+    }
+
     render() {
         return (
             <div className="NewPost">
