2c608d54b12d746fd22e603217eea9a770006125
sohagmahin <sohagmahin@gmail.com>
2021-02-21 23:59:34 +0600

    L11 v:18 using switch to single route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Blog.css';
 import Posts from '../Blog/Posts/Posts';
 import NewPost from '../Blog/NewPost/NewPost';
-import { Route, Link, NavLink } from 'react-router-dom';
+import { Route, Link, NavLink, Switch } from 'react-router-dom';
 import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
@@ -30,11 +30,11 @@ class Blog extends Component {
                     </nav>
                 </header>
 
-                {/* <Route path="/" render={() => <h1>Hello</h1>} />
-                <Route path="/" exact render={() => <h1>Hello 2</h1>} /> */}
-                {<Route path="/" exact component={Posts} />}
-                {<Route path="/:id" exact component={FullPost} />}
-                {<Route path="/new-post" component={NewPost} />}
+                <Switch>
+                    {<Route path="/" exact component={Posts} />}
+                    {<Route path="/new-post" component={NewPost} />}
+                    {<Route path="/:id" exact component={FullPost} />}
+                </Switch>
             </div>
 
         );
