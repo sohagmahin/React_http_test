e6363d7474a206b79e980f2f1a017af9cf9f2741
sohagmahin <sohagmahin@gmail.com>
2021-02-24 16:48:14 +0600

    l11 v28 handling 404 cases (unknown route)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -32,7 +32,8 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>Not found!</h1>} />
+                    {/* <Redirect from="/" to="/posts" /> */}
                 </Switch>
             </div>
 
